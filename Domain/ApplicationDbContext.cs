bc1da0633f2b025d350607c2d458695f9da3cb60
vhmd0 <157473087+vhmd0@users.noreply.github.com>
2025-08-16 13:56:26 -0400

    Chore: Add DbSet's

--- Domain/ApplicationDbContext.cs
+++ Domain/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using bankstan_payment.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace bankstan_payment.Domain;
@@ -7,6 +8,15 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+    public DbSet<TransactionType> TransactionTypes => Set<TransactionType>();
+    public DbSet<RiskFlag> RiskFlags => Set<RiskFlag>();
+    public DbSet<Notifications> Notifications => Set<Notifications>();
+    public DbSet<TransactionStatus> TransactionStatus => Set<TransactionStatus>();
+    public DbSet<ServicesProviders> ServicesProviders => Set<ServicesProviders>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {

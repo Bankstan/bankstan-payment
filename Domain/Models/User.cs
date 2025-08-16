4af506ebe8862e043c1c5856e29f2967403bd9ca
vhmd0 <157473087+vhmd0@users.noreply.github.com>
2025-08-16 12:17:53 -0400

    Chore(database): create user_table

---
+++ Domain/Models/User.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using bankstan_payment.Domain.Enum;
+
+namespace bankstan_payment.Domain.Models;
+
+/// <summary>
+/// User personal information and login credentials
+/// </summary>
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public Guid Uuid { get; set; } = Guid.NewGuid();
+
+    [Required] public string FirstName { get; set; } = string.Empty;
+
+    [Required] public string MiddleName { get; set; } = string.Empty;
+
+    [Required] public string LastName { get; set; } = string.Empty;
+
+    [Required] [EmailAddress] public string Email { get; set; } = string.Empty;
+
+    public string ProfilePicture { get; set; } = string.Empty;
+
+    public Gender Gender { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    [Phone] public string Phone { get; set; } = string.Empty;
+
+    [Required] public string PasswordHash { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    [InverseProperty("User")] public ICollection<Account> Accounts { get; set; } = new List<Account>();
+}

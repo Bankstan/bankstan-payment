44c25af83d9cecf6bddb737641a298fdf90dc7ae
vhmd0 <157473087+vhmd0@users.noreply.github.com>
2025-08-16 08:37:01 -0400

    Setup DATABASE_URL

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Replace placeholder with actual secret
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+connectionString = connectionString.Replace("${DATABASE_URL}", Environment.GetEnvironmentVariable("DATABASE_URL"));
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,28 +19,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
